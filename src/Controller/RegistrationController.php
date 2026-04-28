260b53c1a1ec632706bf5c9379ce5092e1606000
Deenoji <braumerandreas@gmail.com>
2026-04-28 19:57:03 +0200

    feat: add registration & login

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,11 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Question;
+use App\Entity\Category;
+use App\Entity\Option;
+use App\Entity\Survey;
 
 final class RegistrationController extends AbstractController
 {
-    #[Route('/registration', name: 'registration')]
+    #[Route('/registration', name: 'registration_index')]
     public function index(): Response
+    {
+        return $this->render('page/content/registration.html.twig', [
+            'controller_name' => 'RegistrationController',
+        ]);
+    }
+
+    #[Route('/registration', name: 'login', methods: ['POST'])]
+    public function login(): Response
+    {
+        return $this->render('page/content/registration.html.twig', [
+            'controller_name' => 'RegistrationController',
+        ]);
+    }
+
+    #[Route('/registration', name: 'registration', methods: ['POST'])]
+    public function registration(): Response
     {
         return $this->render('page/content/registration.html.twig', [
             'controller_name' => 'RegistrationController',
